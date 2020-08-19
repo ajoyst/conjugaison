9e0aa90fdb5c8b03159e0de783394849edd82a4c
Joy\Joy Stephens <alice.joystephens@gmail.com>
2020-08-19 15:51:40 -0500

    +made an apply button to test filters and ran a consol.log to check

--- public/script.js
+++ public/script.js
@@ -9,8 +9,15 @@ function makeVisible(id) {
   document.getElementById(id).style.display = "block";
 }
 
+/*make applybutton function
+var apply = document.getElementById("applyButton");
+apply.addEventListener("click", applyFilter);
+function applyFilter() {
+  console.log("a filter was picked");
+}*/
+
 // overview button to overview page function
 //reviewButton to begin review sequence
 //quizButton to begin quiz
 
-console.log("no errors yet");
+//console.log("no errors yet");
